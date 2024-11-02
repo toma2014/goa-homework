93ad5e7837a8ef2dd648659bef0e5ba6c521bd04
toma2014 <tomakamushadze111@gmail.com>
2024-11-03 00:25:50 +0400

    second commit

---
+++ day8/classwork/hffhjffu.py
@@ -0,0 +1,28 @@
+print(5 + 5)
+print(5 + 6)
+print(9*5)
+print(5*4)
+print(6-3)
+print(9-6)
+print(6/2)
+print(9/6)
+
+
+
+
+
+
+name = "toma"
+print(name)
+age = 10
+print(age)
+num1 = 60
+num1 = 30
+print(num1)
+
+
+
+sport = "football"
+sport = "basketball"
+sport = "valriball"
+print(sport)
